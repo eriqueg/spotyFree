a5eb5bc566e928c7e83e0b1a98281181c375d687
eriqueg <guizanoni1234@gmail.com>
2019-10-25 19:55:09 -0300

    Criando primeiros components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }];
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent}
+];
 
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegistryComponent } from './registry/registry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegistryComponent
   ],
   imports: [
     BrowserModule,
